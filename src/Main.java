b8c6743b8472694a66556b9629051b862100f268
Lenovo <tumenbajermuha@gmail.com>
2026-03-20 23:21:31 +0600

    first commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    static void printDigits(int n) {
+        if (n == 0) return;
+        printDigits(n / 10);
+        System.out.println(n % 10);
+    }
+
+    public static void main(String[] args) {
+
+        printDigits(5481);
+    }
+}
